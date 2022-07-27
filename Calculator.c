502c746dd8ed782ac06cceece298434d8473dcbb
NishitaLath <95167563+NishitaLath@users.noreply.github.com>
2022-07-27 19:39:03 +0530

    Taking input from user

---
+++ Calculator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//Addition, subtraction, multiplication, division
+int main(void)
+{
+	float a, b; int op;
+	printf("Enter the first number\n");
+	scanf("%f",&a);
+	printf("Enter the second number\n");
+	scanf("%f",&b);
+	//Choosing the operation to be performed
+	printf("Enter 1 for addition \n");
+	printf("Enter 2 for subtraction \n");
+	printf("Enter 3 for multiplication \n");
+	printf("Enter 4 for division \n");
+	scanf("%d",&op);
+	
+}
