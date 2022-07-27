4e5cf2cde3aaa92b628f619b61af5379a54dc044
NishitaLath <95167563+NishitaLath@users.noreply.github.com>
2022-07-27 19:42:42 +0530

    Addition Operation

--- Calculator.c
+++ Calculator.c
@@ -13,5 +13,26 @@ int main(void)
 	printf("Enter 3 for multiplication \n");
 	printf("Enter 4 for division \n");
 	scanf("%d",&op);
-	
+	//A number can not be divided by zero
+	if(op==4 && b==0)
+	{
+		printf("Division cannot be performed");
+	}
+	//Performing the operations
+	else if(op==1 || op==2 || op==3 || op==4)
+	{
+		float res;
+		switch(op)
+		{
+			case 1:
+			res=a+b;
+			break;
+		}
+		printf("The result is= %f \n", res);
+	}
+	else
+	{
+		printf("Invalid Operator");
+	}
+	return 0;
 }
